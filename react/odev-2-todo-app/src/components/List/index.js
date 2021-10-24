c1cd5a18a046126864eccb3e603329041c7ab9ad
Samet Koyuncu <sametkoyuncu@live.com>
2021-10-24 15:25:01 +0300

    Add classes to checked item

--- react/odev-2-todo-app/src/components/List/index.js
+++ react/odev-2-todo-app/src/components/List/index.js
@@ -1,6 +1,19 @@
 import React from 'react'
 //This section should be hidden by default and shown when there are todos
-function List({ todos }) {
+function List({ todos, updateTodos }) {
+  const toggleCheckbox = (e) => {
+    //add class for label
+    e.target.classList.toggle('checked')
+    //add class for parent li
+    e.target.parentElement.parentElement.classList.toggle('completed')
+    //change status to task item
+    todos[e.target.value].status === 'active'
+      ? (todos[e.target.value].status = 'completed')
+      : (todos[e.target.value].status = 'active')
+
+    updateTodos([...todos])
+  }
+
   return (
     <section className="main">
       <input className="toggle-all" type="checkbox" />
@@ -29,10 +42,20 @@ function List({ todos }) {
           </div>
         </li>
         {todos.map((item, index) => (
-          <li key={index}>
+          <li
+            key={index}
+            className={item.status == 'completed' ? 'completed' : ''}
+          >
             <div className="view">
-              <input className="toggle" type="checkbox" />
-              <label>{item}</label>
+              <input
+                className={
+                  item.status == 'completed' ? 'toggle checked' : 'toggle'
+                }
+                onChange={toggleCheckbox}
+                value={index}
+                type="checkbox"
+              />
+              <label>{item.task}</label>
               <button className="destroy"></button>
             </div>
           </li>
